3ec3f95806aa876d4bbca548ea64d31734921c34
panther-swap <admin@pantherswap.com>
2021-04-01 11:02:17 +0800

    refactor: Disable theme switch

--- src/ThemeContext.tsx
+++ src/ThemeContext.tsx
@@ -13,14 +13,15 @@ const ThemeContext = React.createContext<ThemeContextType>({ isDark: false, togg
 
 const ThemeContextProvider: React.FC = ({ children }) => {
   const [isDark, setIsDark] = useState(() => {
-    const isDarkUserSetting = localStorage.getItem(CACHE_KEY)
-    return isDarkUserSetting ? JSON.parse(isDarkUserSetting) : false
+    // const isDarkUserSetting = localStorage.getItem(CACHE_KEY)
+    // return isDarkUserSetting ? JSON.parse(isDarkUserSetting) : false
+    return true;
   })
 
   const toggleTheme = () => {
     setIsDark((prevState: any) => {
       localStorage.setItem(CACHE_KEY, JSON.stringify(!prevState))
-      return !prevState
+      return !prevState || true;
     })
   }
 
