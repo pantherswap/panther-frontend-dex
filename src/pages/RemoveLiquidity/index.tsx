f563e495574189d97cacf2163fd9727dfeb35cbf
panther-swap <admin@pantherswap.com>
2021-05-06 15:18:37 +0800

    refactor: Update LP token name

--- src/pages/RemoveLiquidity/index.tsx
+++ src/pages/RemoveLiquidity/index.tsx
@@ -123,7 +123,7 @@ export default function RemoveLiquidity({
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'Pancake LPs',
+      name: 'PantherSwap LPs',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address,
