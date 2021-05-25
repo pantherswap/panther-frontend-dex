298189ffeac71412ab348cfa84a4246c342b3a4e
p-wtt <parksangmin.wyatt@gmail.com>
2021-05-25 18:31:09 +0900

    init landing

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from "styled-components"
+
+const Landing = () => {
+  return (
+    <TEST>
+      안녕하세요!
+    </TEST>
+  );
+}
+
+export default Landing
+
+const TEST = styled.div`
+    background-color: #FFFFFF;
+`
