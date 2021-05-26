f77c0e375ee262012e88df2d7c73ad11170602e5
panther-swap <admin@pantherswap.com>
2021-05-26 11:10:23 +0800

    fix: Update theme name

--- src/style/styled.d.ts
+++ src/style/styled.d.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-unresolved
-import { PancakeTheme } from '@pantherswap-libs/uikit/dist/theme'
+import { PantherTheme } from '@pantherswap-libs/uikit/dist/theme'
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
-  export interface DefaultTheme extends PancakeTheme {}
+  export interface DefaultTheme extends PantherTheme {}
 }
