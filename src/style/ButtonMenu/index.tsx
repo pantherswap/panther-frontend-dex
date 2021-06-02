7c2123e38fefb1e9ce17d0cdb7bce3b2ab53041c
p-wtt <parksangmin.wyatt@gmail.com>
2021-06-02 21:08:29 +0900

    delte local theme

--- src/style/ButtonMenu/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as ButtonMenu } from "./ButtonMenu";
-export { default as ButtonMenuItem } from "./ButtonMenuItem";
-export type { ButtonMenuProps, ButtonMenuItemProps } from "./types";
